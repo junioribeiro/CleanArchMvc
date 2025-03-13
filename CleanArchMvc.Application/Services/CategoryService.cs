124e9f32791a5e4d21029c0448b281e9a5222960
Junio Ribeiro <ads.junio@hotmail.com>
2025-03-13 17:22:15 -0300

    implements Application and webUI

---
+++ CleanArchMvc.Application/Services/CategoryService.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+
+namespace CleanArchMvc.Application.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CategoryService(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public async Task AddAsync(CategoryDTO categoryDto)
+        {
+            var entity = _mapper.Map<Category>(categoryDto);
+            await _categoryRepository.CreateAsync(entity);
+        }
+
+        public async Task<CategoryDTO?> GetByIdAsync(int id)
+        {
+            var entity = await _categoryRepository.GetByIdAsync(id);
+            return _mapper.Map<CategoryDTO>(entity);
+        }
+
+        public async Task<IEnumerable<CategoryDTO>> GetCategoriesAsync()
+        {
+            var entities = await _categoryRepository.GetCategoriesAsync();
+            return _mapper.Map<IEnumerable<CategoryDTO>>(entities);
+        }
+
+        public async Task RemoveAsync(int id)
+        {
+            var entity = await _categoryRepository.GetByIdAsync(id);
+            await _categoryRepository.RemoveAsync(entity);
+        }
+
+        public async Task UpdateAsync(CategoryDTO categoryDto)
+        {
+            var entity = _mapper.Map<Category>(categoryDto);
+            await _categoryRepository.UpdateAsync(entity);
+        }
+    }
+}
---
+++ CleanArchMvc.Application/Services/ProductService.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+
+        public ProductService(IProductRepository productRepository, IMapper mapper)
+        {
+            _productRepository = productRepository;
+            _mapper = mapper;
+        }
+
+        public async Task AddAsync(ProductDTO productDto) 
+        {
+            var entity = _mapper.Map<Product>(productDto);
+            await _productRepository.CreateAsync(entity);
+        }
+
+        public async Task<ProductDTO> GetByIdAsync(int id)
+        {
+            var entity = await _productRepository.GetByIdAsync(id);
+            return _mapper.Map<ProductDTO>(entity);
+        }
+
+        public async Task<ProductDTO> GetProductCategoryAsync(int id)
+        {
+            var entity = await _productRepository.GetProductCategoryAsync(id);
+            return _mapper.Map<ProductDTO>(entity);
+        }
+
+        public async Task<IEnumerable<ProductDTO>> GetProductsAsync()
+        {
+            var entities = await _productRepository.GetProductsAsync();
+            return _mapper.Map<IEnumerable<ProductDTO>>(entities);
+        }
+
+        public async Task RemoveAsync(int id)
+        {
+            var entity = await _productRepository.GetByIdAsync(id);
+            await _productRepository.RemoveAsync(entity);
+        }
+
+        public async Task UpdateAsync(ProductDTO productDto)
+        {
+            var entity = _mapper.Map<Product>(productDto);
+            await _productRepository.UpdateAsync(entity);
+        }
+    }
+}
